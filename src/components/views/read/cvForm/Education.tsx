67b168d0d798f2331be4cc2a4f859a44290195c6
Ameer <aaareatawfik@gmail.com>
2023-06-08 14:15:17 +0200

    fix: change the rendering of education section

--- src/components/views/read/cvForm/Education.tsx
+++ src/components/views/read/cvForm/Education.tsx
@@ -87,6 +87,9 @@ const Education: FC<Props> = ({
   const [inputValues, setInputValues] = useState<
     { cardId: number; values: { [key: string]: string } }[]
   >([]);
+  const [inputValuesSecondary, setInputValuesSecondary] = useState<
+    { [key: string]: string }[]
+  >([]);
   const handleChange = (cardId: number, field: string, value: string): void => {
     setInputValues((prevInputValues) => {
       const cardIndex = prevInputValues.findIndex(
@@ -106,6 +109,7 @@ const Education: FC<Props> = ({
             values: { ...inputValue.values, [field]: value },
           };
         }
+        console.log(inputValue);
         return inputValue;
       });
     });
@@ -113,6 +117,15 @@ const Education: FC<Props> = ({
 
   const [startDate, setStartDate] = useState<Dayjs | null>(dayjs('2022-04-17'));
   const [endDate, setEndDate] = useState<Dayjs | null>(dayjs('2022-04-18'));
+  const mapping = [
+    { key: 'degree', label: 'Degree' },
+    { key: 'institutionName', label: 'Institution Name' },
+    { key: 'major', label: 'Major' },
+    { key: 'startDate', label: 'Start Date' },
+    { key: 'endDate', label: 'End Date' },
+    { key: 'gpa', label: 'GPA' },
+    { key: 'country', label: 'Country' },
+  ];
   const handleNext = (): void => {
     const modifiedValues = cards.map((card) => {
       const cardInputValues = inputValues.find(
@@ -134,22 +147,30 @@ const Education: FC<Props> = ({
     nextPage();
     nextStep();
   };
+  const handleChangeSecondary = (field: string, value: string): void => {
+    setInputValuesSecondary((prevInputValues) => {
+      const updatedInputValues = [...prevInputValues];
+      const fieldValue = { [field]: value };
+      const existingFieldIndex = updatedInputValues.findIndex(
+        (inputValue) => Object.keys(inputValue)[0] === field,
+      );
+      if (existingFieldIndex !== -1) {
+        // Field already exists, update its value
+        updatedInputValues[existingFieldIndex] = fieldValue;
+      } else {
+        // Field doesn't exist, add it to the input values
+        updatedInputValues.push(fieldValue);
+      }
+      console.log(updatedInputValues);
+      return updatedInputValues;
+    });
+  };
   return (
     <div>
       <h2>Education</h2>
       <div>
         {cards.map((card, index) => (
-          <Card
-            key={card.id}
-            sx={{
-              maxWidth: 900,
-              position: 'relative',
-              zIndex: 100,
-              height: '400px',
-              overflow: 'auto',
-            }}
-            className={`card-item card-${card.id}`}
-          >
+          <Card key={card.id}>
             <CardContent>
               <Typography gutterBottom variant="h5" component="div">
                 Education
@@ -159,133 +180,147 @@ const Education: FC<Props> = ({
               </Typography>
               {showFields[index] && (
                 <>
-                  <TextField
-                    id={`select-degree-${card.id}`}
-                    select
-                    label="Degree"
-                    value={
-                      inputValues.find(
-                        (inputValue) => inputValue.cardId === card.id,
-                      )?.values.degree || ''
-                    }
-                    onChange={(e) =>
-                      handleChange(card.id, 'degree', e.target.value)
-                    }
-                    required
-                    helperText="Please select your degree"
-                    margin="normal"
-                  >
-                    <MenuItem value="Diploma">Diploma</MenuItem>
-                    <MenuItem value="Bachelors">Bachelors</MenuItem>
-                    <MenuItem value="Master">Master</MenuItem>
-                    <MenuItem value="PhD">PhD</MenuItem>
-                    <MenuItem value="Internship - Training">
-                      Internship - Training
-                    </MenuItem>
-                  </TextField>
-                  <TextField
-                    label="Institution Name"
-                    value={
-                      inputValues.find(
-                        (inputValue) => inputValue.cardId === card.id,
-                      )?.values.instName || ''
-                    }
-                    onChange={(e) =>
-                      handleChange(card.id, 'instName', e.target.value)
-                    }
-                    required
-                  />
-                  <TextField
-                    label="Major - Field of Study"
-                    value={
-                      inputValues.find(
-                        (inputValue) => inputValue.cardId === card.id,
-                      )?.values.major || ''
-                    }
-                    onChange={(e) =>
-                      handleChange(card.id, 'major', e.target.value)
-                    }
-                    required
-                  />
-                  <LocalizationProvider dateAdapter={AdapterDayjs}>
-                    <DatePicker
-                      label="From"
-                      value={
-                        inputValues.find(
-                          (inputValue) => inputValue.cardId === card.id,
-                        )?.values.startDate || startDate
-                      }
-                      maxDate={dayjs()}
-                      onChange={(date) => {
-                        handleChange(
-                          card.id,
-                          'startDate',
-                          date
-                            ? dayjs(date).format('YYYY-MM-DD')
-                            : dayjs(startDate).format('YYYY-MM-DD'),
-                        );
-                        setStartDate(dayjs(date));
-                      }}
-                    />
-                  </LocalizationProvider>
-                  <LocalizationProvider dateAdapter={AdapterDayjs}>
-                    <DatePicker
-                      label="Till"
-                      value={
-                        inputValues.find(
-                          (inputValue) => inputValue.cardId === card.id,
-                        )?.values.endDate || endDate
-                      }
-                      minDate={startDate}
-                      maxDate={dayjs()}
-                      onChange={(date) =>
-                        handleChange(
-                          card.id,
-                          'endDate',
-                          date
-                            ? dayjs(date).format('YYYY-MM-DD')
-                            : dayjs(endDate).format('YYYY-MM-DD'),
-                        )
-                      }
-                    />
-                  </LocalizationProvider>
-                  <TextField
-                    label="GPA"
-                    value={
-                      inputValues.find(
-                        (inputValue) => inputValue.cardId === card.id,
-                      )?.values.gpa || ''
-                    }
-                    onChange={(e) =>
-                      handleChange(card.id, 'gpa', e.target.value)
-                    }
-                  />
-                  <TextField
-                    id={`select-country-${card.id}`}
-                    select
-                    label="Country"
-                    value={
-                      inputValues.find(
-                        (inputValue) => inputValue.cardId === card.id,
-                      )?.values.country || ''
-                    }
-                    onChange={(e) =>
-                      handleChange(card.id, 'country', e.target.value)
-                    }
-                    required
-                    helperText="Please select your country"
-                    margin="normal"
-                  >
-                    <MenuItem value="Switzerland">Switzerland</MenuItem>
-                    <MenuItem value="Europe">Europe</MenuItem>
-                    <MenuItem value="Asia">Asia</MenuItem>
-                    <MenuItem value="Africa">Africa</MenuItem>
-                    <MenuItem value="North America">North America</MenuItem>
-                    <MenuItem value="South America">South America</MenuItem>
-                    <MenuItem value="Other">Other</MenuItem>
-                  </TextField>
+                  {mapping.map((m) => (
+                    <>
+                      <p>{m.label}</p>
+                      {m.key !== 'degree' &&
+                      m.key !== 'startDate' &&
+                      m.key !== 'endDate' &&
+                      m.key !== 'country' ? (
+                        <TextField
+                          label={m.label}
+                          id={m.key}
+                          value={
+                            inputValues.find(
+                              (inputValue) => inputValue.cardId === card.id,
+                            )?.values[m.key] || ''
+                          }
+                          onChange={(e) =>
+                            handleChange(card.id, m.key, e.target.value)
+                          }
+                          required
+                        />
+                      ) : (
+                        <>
+                          {m.key === 'degree' && (
+                            <TextField
+                              id="select-degree"
+                              select
+                              label="Degree"
+                              value={
+                                inputValues.find(
+                                  (inputValue) => inputValue.cardId === card.id,
+                                )?.values.degree || ''
+                                // inputValuesSecondary.find(
+                                //   (inputValue) =>
+                                //     Object.keys(inputValue)[0] === 'degree',
+                                // )?.degree || ''
+                              }
+                              onChange={(e) =>
+                                handleChange(card.id, 'degree', e.target.value)
+                              }
+                              required
+                              helperText="Please select your degree"
+                              margin="normal"
+                            >
+                              <MenuItem value="bachelor">Bachelor</MenuItem>
+                              <MenuItem value="master">Master</MenuItem>
+                              <MenuItem value="phd">PhD</MenuItem>
+                            </TextField>
+                          )}
+                          {m.key === 'startDate' && (
+                            <LocalizationProvider dateAdapter={AdapterDayjs}>
+                              <DatePicker
+                                label="From"
+                                value={
+                                  // startDate
+                                  inputValues.find(
+                                    (inputValue) =>
+                                      inputValue.cardId === card.id,
+                                  )?.values.startDate || startDate
+                                }
+                                maxDate={dayjs()}
+                                onChange={(date) => {
+                                  handleChange(
+                                    card.id,
+                                    'startDate',
+                                    date
+                                      ? dayjs(date).format('YYYY-MM-DD')
+                                      : dayjs(startDate).format('YYYY-MM-DD'),
+                                  );
+                                  setStartDate(dayjs(date));
+                                }}
+                              />
+                            </LocalizationProvider>
+                          )}
+                          {m.key === 'endDate' && (
+                            <LocalizationProvider dateAdapter={AdapterDayjs}>
+                              <DatePicker
+                                label="Till"
+                                value={
+                                  // endDate
+                                  inputValues.find(
+                                    (inputValue) =>
+                                      inputValue.cardId === card.id,
+                                  )?.values.endDate || endDate
+                                }
+                                minDate={startDate}
+                                maxDate={dayjs()}
+                                onChange={(date) => {
+                                  handleChange(
+                                    card.id,
+                                    'endDate',
+                                    date
+                                      ? dayjs(date).format('YYYY-MM-DD')
+                                      : dayjs(endDate).format('YYYY-MM-DD'),
+                                  );
+                                  setEndDate(dayjs(date));
+                                }}
+                              />
+                            </LocalizationProvider>
+                          )}
+                          {m.key === 'country' && (
+                            <TextField
+                              id="select-country"
+                              select
+                              label="Country"
+                              value={
+                                inputValues.find(
+                                  (inputValue) => inputValue.cardId === card.id,
+                                )?.values.country || ''
+                                // inputValuesSecondary.find(
+                                //   (inputValue) =>
+                                //     Object.keys(inputValue)[0] === 'country',
+                                // )?.country || ''
+                              }
+                              onChange={(e) =>
+                                handleChange(card.id, 'country', e.target.value)
+                              }
+                              required
+                              helperText="Please select your country"
+                              margin="normal"
+                            >
+                              <MenuItem value="Switzerland">
+                                Switzerland
+                              </MenuItem>
+                              <MenuItem value="Europe">Europe</MenuItem>
+                              <MenuItem value="Asia">Asia</MenuItem>
+                              <MenuItem value="Africa">Africa</MenuItem>
+                              <MenuItem value="North America">
+                                North America
+                              </MenuItem>
+                              <MenuItem value="South America">
+                                South America
+                              </MenuItem>
+                              <MenuItem value="Other">Other</MenuItem>
+                            </TextField>
+                          )}
+                        </>
+                      )}
+                    </>
+                  ))}
                 </>
-              )}
+              )}{' '}
               <CardActions>
                 <Button size="small" startIcon={<Add />} onClick={handleAdd}>
                   Add
@@ -321,7 +356,6 @@ const Education: FC<Props> = ({
         ))}
       </div>
       <Button
-        style={{ position: 'absolute', top: '605px', left: '628px' }}
         variant="contained"
         color="primary"
         startIcon={<NavigateBeforeIcon />}
@@ -330,8 +364,6 @@ const Education: FC<Props> = ({
         Back
       </Button>
       <Button
-        style={{ position: 'absolute', top: '599px', left: '1070px' }}
-        sx={{ width: 165 }}
         variant="contained"
         color="primary"
         startIcon={<NavigateNextIcon />}
